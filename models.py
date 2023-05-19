0cd5314ce3e3ebbd6d9caeab5a02541ba58529b8
godwins3 <praisegodwins4@gmail.com>
2023-05-20 00:30:31 +0300

    adding major classes

--- models.py
+++ models.py
@@ -1,7 +1,69 @@
 import cv2
 
-def main():
-    img = cv2.VideoCapture(0)
+#================================================================================================
+# My Camera object and its settings 
+CAM = cv2.VideoCapture(1)
+CAM.set(cv2.CAP_PROP_FRAME_WIDTH,WIDTH)
+CAM.set(cv2.CAP_PROP_FRAME_HEIGHT,HEIGHT)
+CAM.set(cv2.CAP_PROP_FPS,FPS)
+CAM.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
 
-    return
-main()
+#================================================================================================
+class myHands:
+    import mediapipe as mp
+    def __init__(self,static_image_mode=False, max_num_hands=2, model_complexity=1,min_detection_confidence=0.5,min_tracking_confidence=0.5):
+        self.static_image_mode=static_image_mode
+        self.max_num_hands=max_num_hands
+        self.model_complexity=model_complexity
+        self.min_detection_confidence=min_detection_confidence
+        self.min_tracking_confidence=min_tracking_confidence
+        self.hands = self.mp.solutions.hands.Hands(self.static_image_mode,self.max_num_hands,self.model_complexity,self.min_detection_confidence,self.min_tracking_confidence)
+        self.mpDraw = self.mp.solutions.drawing_utils
+
+
+    def Marks(self,frame):
+        myHands = list() # [[first_hand], [second_hand],[third_hand],[...],...]
+        result = self.hands.process(frame)
+        if (result.multi_hand_landmarks is not None):
+            for hand in result.multi_hand_landmarks:
+                myHand = list()
+                for handLandMarks in hand.landmark:
+                    myHand.append((int(handLandMarks.x * WIDTH),int(handLandMarks.y * HEIGHT)))
+                myHands.append(myHand)
+        return [myHands,result]
+
+
+    def drawConnection(self,rgbFrame,bgrFrame):
+        result = self.Marks(rgbFrame)[1]
+        if result.multi_hand_landmarks is not None:
+            for LandMarks in result.multi_hand_landmarks:
+                    self.mpDraw.draw_landmarks(bgrFrame,LandMarks,self.mp.solutions.hands.HAND_CONNECTIONS)
+
+
+#================================================================================================
+mpHands = myHands()
+class main():
+    def main():
+        while True:
+            isTrue,frame = CAM.read()
+
+            if not(isTrue):
+                break
+            frame = cv2.flip(frame,1)
+            rgbFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            myHands = mpHands.Marks(rgbFrame)
+
+            if len(myHands[0]) >= 1:
+                for hand in myHands[0]:
+                    cv2.rectangle(
+                        frame,
+                        (int(hand[8][0] - PADDLE_WIDTH/2),0), 
+                        (int(hand[8][0] + PADDLE_WIDTH/2),PADDLE_HEIGHT),
+                        (0,255,0),
+                        -1
+                    )
+        CAM.release()   
+
+
+        return
+
